8241b2add7bd7950cad3db4dd63357671392bfbc
Kristina Diaz <kristina.diaz84@gmail.com>
2022-03-22 16:14:05 -0600

    modified startGame function

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-let firstCard = getRandomCard()
-let secondCard = getRandomCard()
-let cards = [firstCard, secondCard]
-let sum = firstCard + secondCard
+let cards = []
+let sum = 0
 let hasBlackJack = false
-let isAlive = true
+let isAlive = false
 let message = ''
 let messageEl = document.getElementById('message-el')
 let sumEl = document.getElementById('sum-el')
@@ -11,7 +9,7 @@ let cardsEl = document.getElementById('cards-el')
 
 function getRandomCard() {
   let randomNumber = Math.floor( Math.random() * 13 ) + 1
-  
+
   if(randomNumber > 10) {
     return 10
   } else if(randomNumber === 1) {
@@ -22,6 +20,11 @@ function getRandomCard() {
 }
 
 function startGame() {
+  isAlive = true
+  let firstCard = getRandomCard()
+  let secondCard = getRandomCard()
+  cards = [firstCard, secondCard]
+  sum = firstCard + secondCard
   renderGame()
 }
 
